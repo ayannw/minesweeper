8955fc974ed5380b130e4f797d006d910d7142bb
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-04-26 12:04:43 +0200

    feat: Added Types

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,20 @@ const random = (max: number): number => {
   return Math.floor(Math.random() * max);
 };
 
-class Cell implements Cell {
+class Cell {
   public hasMine: boolean = false;
 
-  public setMine(v: boolean) {
+  public setMine(v: boolean): void {
     this.hasMine = v;
   }
 }
 
 export class Board {
-  public getBoard() {
+  public getBoard(): Board {
     return this.board;
   }
 
-  public plantMines(dimension, mines) {
+  public plantMines(dimension: number, mines: number): void {
     let mineCount = 0;
     while (mineCount < mines) {
       let x = random(dimension),
@@ -29,12 +29,12 @@ export class Board {
     }
   }
 
-  public countMines() {
-    let count = 0,
-      i,
-      j,
-      row,
-      cell;
+  public countMines(): number {
+    let count = 0;
+    let i: number;
+    let j: number;
+    let row: Array<Cell>;
+    let cell: Cell;
 
     for (i = 0; i < this.board.length; i++) {
       row = this.board[i];
@@ -49,11 +49,11 @@ export class Board {
     return count;
   }
 
-  public board: any[] = new Array();
+  public board: Array<Array<Cell>> = new Array();
 
-  public constructor(dimension: number, mines: number) {
+  public constructor(dimension: number, mines: number): void {
     for (let i = 0; i < dimension; i++) {
-      let row = [];
+      let row: Array<Cell> = [];
       for (let j = 0; j < dimension; j++) {
         row.push(new Cell(false));
       }
